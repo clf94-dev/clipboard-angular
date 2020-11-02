0b81cd0b57d4b14f2a0a1426f4935f87f8f90c0a
clf94-dev <clf94@hotmail.es>
2020-11-02 19:58:52 +0100

    added initial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopComponent } from './components/top/top.component';
+import { MiddleComponent } from './components/middle/middle.component';
+import { CompaniesComponent } from './components/companies/companies.component';
+import { FeaturesComponent } from './components/features/features.component';
+import { CtaComponent } from './components/cta/cta.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopComponent,
+    MiddleComponent,
+    CompaniesComponent,
+    FeaturesComponent,
+    CtaComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
